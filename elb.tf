560869535923495a6410116e7b40d5d3cc5849b0
pranav-admin <154361136+pranav-admin@users.noreply.github.com>
2024-05-05 19:15:18 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -4,3 +4,13 @@ provider "aws" {
 resource "aws_eip" "lb" {
   domain   = "vpc"
 }
+
+resource "aws_instance" "web" {
+  ami           = "ami-0cf2b4e024cdb6960"
+  instance_type = "t2.micro"
+  tags = {
+    name = "my-test-vm"
+  }
+
+
+}
